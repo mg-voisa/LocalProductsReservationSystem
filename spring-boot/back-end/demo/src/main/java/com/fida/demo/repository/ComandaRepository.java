7d3a35aa5d9a9132d0d202baba09757d2e819ab0
AzureAD\GigelVoisa <voisa.gigel@gmail.com>
2023-03-15 08:37:01 +0200

    change on spring-boot

--- spring-boot/back-end/demo/src/main/java/com/fida/demo/repository/ComandaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fida.demo.repository;
-
-public interface ComandaRepository {
-	//crud
-	public void adauga();
-	public void vezi();
-	public void modifica();
-	public void sterge();
-}
--- spring-boot/back-end/demo/src/main/java/com/fida/demo/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fida.demo.repository;
-
-public interface IngredientRepository {
-	//crud	
-	public void adauga();
-	public void vezi();
-	public void modifica();
-	public void sterge();
-}
